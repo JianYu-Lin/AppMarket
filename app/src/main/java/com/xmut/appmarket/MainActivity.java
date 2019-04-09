5391f41b49fa85e6785a64ace5edceef62f572d7
LJY <121682493@qq.com>
2019-04-09 22:58:05 +0800

    按两次返回退出程序

--- app/src/main/java/com/xmut/appmarket/MainActivity.java
+++ app/src/main/java/com/xmut/appmarket/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private FragmentOne fragmentOne;
     private FragmentTwo fragmentTwo;
     private FragmentThree fragmentThree;
+    long firstTime = System.currentTimeMillis();
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -42,9 +45,24 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    public void onBackPressed() {
+
+        long secondTime = System.currentTimeMillis();
+        if (secondTime - firstTime > 2000) {
+            Toast.makeText(MainActivity.this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            firstTime = secondTime;
+        } else {
+            System.exit(0);
+        }
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null) actionBar.hide();
         init();
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
@@ -58,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction beginTransaction=getFragmentManager().beginTransaction();
         beginTransaction.add(R.id.content,fragmentOne).add(R.id.content,fragmentTwo).add(R.id.content,fragmentThree);//开启一个事务将fragment动态加载到组件
         beginTransaction.hide(fragmentOne).hide(fragmentTwo).hide(fragmentThree);//隐藏fragment
-        beginTransaction.addToBackStack(null);//返回到上一个显示的fragment
+        /*beginTransaction.addToBackStack(null);*///返回到上一个显示的fragment
         beginTransaction.commit();//每一个事务最后操作必须是commit（），否则看不见效果
         showNav(R.id.navigation_home);
     }
@@ -68,25 +86,25 @@ public class MainActivity extends AppCompatActivity {
             case R.id.navigation_home:
                 beginTransaction.hide(fragmentTwo).hide(fragmentThree);
                 beginTransaction.show(fragmentOne);
-                beginTransaction.addToBackStack(null);
+                /*beginTransaction.addToBackStack(null);*/
                 beginTransaction.commit();
                 break;
             case R.id.navigation_dashboard:
                 beginTransaction.hide(fragmentOne).hide(fragmentThree);
                 beginTransaction.show(fragmentTwo);
-                beginTransaction.addToBackStack(null);
+                /*beginTransaction.addToBackStack(null);*/
                 beginTransaction.commit();
                 break;
             case R.id.navigation_search:
                 beginTransaction.hide(fragmentTwo).hide(fragmentOne);
                 beginTransaction.show(fragmentThree);
-                beginTransaction.addToBackStack(null);
+                /*beginTransaction.addToBackStack(null);*/
                 beginTransaction.commit();
                 break;
             case R.id.navigation_My:
                 beginTransaction.hide(fragmentTwo).hide(fragmentOne);
                 beginTransaction.show(fragmentThree);
-                beginTransaction.addToBackStack(null);
+                /*beginTransaction.addToBackStack(null);*/
                 beginTransaction.commit();
                 break;
         }
