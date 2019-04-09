029fe462811bd40ca738d05b1e1ea37a553d9d06
LJY <121682493@qq.com>
2019-04-09 23:10:19 +0800

    在碎片布局内部增加顶部提示信息，而不是在mainactivity的布局里

--- app/src/main/java/com/xmut/appmarket/MainActivity.java
+++ app/src/main/java/com/xmut/appmarket/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+   /* private TextView mTextMessage;*/
     private FragmentOne fragmentOne;
     private FragmentTwo fragmentTwo;
     private FragmentThree fragmentThree;
@@ -24,19 +24,19 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    /*mTextMessage.setText(R.string.title_home);*/
                     showNav(R.id.navigation_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                   /* mTextMessage.setText(R.string.title_dashboard);*/
                     showNav(R.id.navigation_dashboard);
                     return true;
                 case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_notifications);
+                    /*mTextMessage.setText(R.string.title_notifications);*/
                     showNav(R.id.navigation_search);
                     return true;
                 case R.id.navigation_My:
-                    mTextMessage.setText(R.string.app_name);
+                   /* mTextMessage.setText(R.string.app_name);*/
                     showNav(R.id.navigation_My);
             }
             return false;
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if(actionBar!=null) actionBar.hide();
         init();
-        mTextMessage = (TextView) findViewById(R.id.message);
+        /*mTextMessage = (TextView) findViewById(R.id.message);*/
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
